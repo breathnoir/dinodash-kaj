e674049a1edfbc7a35afcb4998499abd9e1fcf8f
Nadezhda <zakopnad@fel.cvut.cz>
2024-05-31 22:30:42 +0200

    sw

--- game/scripts/setup.js
+++ game/scripts/setup.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
-=======
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        navigator.serviceWorker.register('https://breathnoir.github.io/dinodash-kaj/game/sw.js').then(function(registration) {
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function(err) {
             console.log('ServiceWorker registration failed: ', err);
@@ -25,7 +23,6 @@ window.addEventListener('offline', updateOnlineStatus);
 
 updateOnlineStatus();
 
->>>>>>> 989178e (serviceworker)
 var DinoGame = DinoGame || {};
 
 DinoGame.music = new Audio('assets/music.mp3');
